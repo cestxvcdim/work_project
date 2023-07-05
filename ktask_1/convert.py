591334ec4b514adda67ce72032b8a75f82eb8b7c
vvvadim <v.vladimirov@it-park.tech>
2023-07-05 20:34:02 +0300

    Initial commit

---
+++ ktask_1/convert.py
@@ -0,0 +1,47 @@
+import csv
+import json
+
+
+def from_csv_to_json(csv_path, json_path):
+    with open(csv_path, 'r', encoding='utf-8') as fp:
+        reader = csv.reader(fp, delimiter=',', quotechar='"')
+        next(reader, None)  # skip the headers
+
+        data = [row for row in reader]
+
+    data.sort(key=lambda x: x[1])
+
+    data = [
+        {
+            'name': item[1] + ' ' + item[0],
+            'text': item[3],
+            'number': i + 2
+        }
+        for i, item in enumerate(data) if item[0]]
+
+    children_data = {}
+
+    for item in data:
+        if item['name'] not in children_data:
+            children_data[item['name']] = [item['text']]
+        else:
+            children_data[item['name']].append(item['text'])
+
+    result = {}
+    current_id = 10
+    # current_id = 1400
+    # parent_id = 1154
+
+    for k, v in children_data.items():
+
+        result[current_id] = {
+            'id': current_id,
+            'name': k,
+            'children': v
+        }
+
+        current_id += 1
+
+    with open(json_path, 'w', encoding='utf-8') as fp:
+        json_data = json.dumps(result, ensure_ascii=False, indent=4)
+        fp.write(json_data)
---
+++ ktask_1/main.py
@@ -0,0 +1,5 @@
+from ktask_1.config import Config
+from ktask_1.convert import from_csv_to_json
+
+
+from_csv_to_json(Config.TMP_PATH, Config.JSON_PATH)
